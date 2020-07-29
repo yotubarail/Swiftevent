c7e6ef1731d0f4af6eaf0dd755d62a033cb3d4b1
Shota Takinami <shota_t@i.softbank.jp>
2020-07-29 09:23:00 +0900

    Initial Commit

---
+++ Swiftevent/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swiftevent
+//
+//  Created by 滝浪翔太 on 2020/07/29.
+//  Copyright © 2020 滝浪翔太. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
